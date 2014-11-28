6655d95482d2211def953c905dc9cdeedd5cc527
hanz <xiaoyaoyangeren@163.com>
2014-11-28 20:28:21 +0800

    flush

--- FlushScript.py
+++ FlushScript.py
@@ -3,66 +3,48 @@ __author__ = 'yer'
 import re
 import requests
 import yaml
-power_dict = []
-keyword_dict = []
-pageid_dict = []
-ig_id_dict = []
-def flush301(keywords):
-    menhu = {}
+import json
 
+def flush301(keywords, ip, flushurl):
     try:
         res = requests.get("http://www.zhongsou.net/%s" % keywords)
         res_data = res.content
-        power = re.findall(r'g_widgetSerial=".*"', res_data)[0]
+        powerstr = re.findall(r'g_widgetSerial=".*"', res_data)[0]
         keyword =  re.findall(r'g_keyword = ".*"', res_data)[0]
         pageid =  re.findall(r'g_curPageNum = .*;', res_data)[0]
         ig_id =  re.findall(r'g_owner=".*"', res_data)[0]
-        # print res_data
-        # print power,keyword,pageid,ig_id
-        # power_dict.append(power.split('"')[1])
-        # print power_dict
-        # print power.split('"')[1]
-        # print keyword.split('"')[1]
-        # print pageid.split(' ')[2][0:-1]
-        # print ig_id.split('"')[1]
-        # print "------------------"
-        power_dict = (power.split('"')[1])
+        power_dict = (powerstr.split('"')[1])
         keyword_dict = (keyword.split('"')[1])
         pageid_dict = (pageid.split(' ')[2][0:-1])
         ig_id_dict = (ig_id.split('"')[1])
-        print power_dict, keyword_dict, pageid_dict, ig_id_dict
+        print keyword_dict, ig_id_dict, pageid_dict, power_dict
+        for x in ip:
+            data = {
+                "ip" : x,
+                "port" : 2047,
+                "jsonStr" : "",
+                "pr" : "",
+                "pageid" : pageid_dict,
+                "press" : "ok"
+            }
+        po = requests.post(flushurl, data=json.dumps(data))
+        # po = requests.post(flush_url, data=ss)
+        # print po.content
     except:
         print u"没有这个门户词：" + keywords
 
-
-    # print "keyword:"+keyword
-    # print "pageid:"+pageid
-    # print "ig_id:"+ig_id
-
 def readyml():
-    s = yaml.load(file('keyword.yml'))
-    a = s['keyword'].split(' ')
-    # print a
-    # print a.split(' ')
-    # for x in a.split(' '):
-    #     print x
-    # print type(a)
-    # print type(s['host']['ip01'])
-    return a
+    readres = yaml.load(file('keyword.yml'))
+    keyword = readres['keyword'].split(' ')
+    np = readres['ip']['nginx'].split(' ')
+    pageflush = readres['url']['b2b']['pageflush']
+    ipflush = readres['url']['b2b']['ipflush'].split(' ')
+    return keyword, np, pageflush, ipflush
 
 
 if __name__=="__main__":
-    # keywords = raw_input("请输入门户词：\n")
-    # k = readyml()
-    # print k
-    # a = []
-    # b = []
-    # c = []
-    # d = []
-    for x in readyml():
-        # print x
-        flush301(x)
-
-    # readyml()
+    keyword, np, pageflush, ipflush = readyml()
+    for x in keyword:
+        flush301(x, ipflush, pageflush)
 
 
