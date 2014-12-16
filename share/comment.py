a50b9e477f075749d28fdca6737e09c4a603c3fd
hanz <xiaoyaoyangeren@163.com>
2014-12-16 13:22:39 +0800

    删除展会信息

--- share/comment.py
+++ share/comment.py
@@ -1,1 +1,11 @@
 __author__ = 'hanz'
+#*--coding=utf-8--*
+import requests
+import os
+import re
+
+def geturl():
+    pass
+    url = 'http://api2.souyue.mobi/d3api2/webdata/homepage.news.groovy?vc=4.0.2&type=interest&keyword=%E6%90%8F%E5%87%BB%E5%9C%88&srpId=68&lastId=0&token=d2a4ab51-240b-42db-a0de-181cc413bdac '
+    a = requests.get(url)
+    a.content
