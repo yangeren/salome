f16813e6cd23e11ba80ccf67dacb9e93bb3b3ec3
yer <xiaoyaoyangeren@163.com>
2014-12-16 23:26:42 +0800

    ++

---
+++ my_flask/my_flask.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
